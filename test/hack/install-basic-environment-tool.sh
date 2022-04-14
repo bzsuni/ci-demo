f303efe0172c1d67ffe7f3c6db15b5f94afb6c81
bzsuni <bingzhe.sun@daocloud.io>
2022-04-14 11:56:30 +0800

    install go

--- test/hack/install-basic-environment-tool.sh
+++ test/hack/install-basic-environment-tool.sh
@@ -4,10 +4,32 @@ function msg() {
    if [[ $# -ne 1 ]]; then echo "[func msg] one arg needed"; exit 1; fi
     echo -e "\033[35m $1 \033[0m"
 }
+function err() {
+   if [[ $# -ne 1 ]]; then echo "[func err] one arg needed"; exit 1; fi
+   echo -e "\033[31m $1 \033[0m"
+}
 # install go
 msg "## install go"
 if ! $(which go > /dev/null 2>&1); then
-  echo need to install go
+  sudo wget "https://golang.google.cn/dl/go1.18.linux-amd64.tar.gz"
+  sudo tar -C /usr/local -xzf go1.18.linux-amd64.tar.gz
+  sudo rm -rf go1.18.linux-amd64.tar.gz
+  sudo go env -w GOPROXY=https://goproxy.cn,direct
+  sudo go env -w GO111MODULE=on
+
+  if ! $(cat $HOME/.bashrc | grep GOROOT > /dev/null 2>&1); then
+    sudo echo -e "export GOROOT=/usr/local/go" >> $HOME/.bashrc
+  fi
+  if ! $(cat $HOME/.bashrc | grep GOPATH > /dev/null 2>&1); then
+    sudo echo -e "export GOPATH=$HOME/go" >> $HOME/.bashrc
+  fi
+  if ! $(cat $HOME/.bashrc | grep GOBIN > /dev/null 2>&1); then
+    sudo echo -e "export GOBIN=$GOPATH/bin" >> $HOME/.bashrc
+  fi
+  if ! $(cat $HOME/.bashrc | grep PATH | grep GOPATH > /dev/null 2>&1); then
+    sudo echo -e 'export PATH=$GOPATH:$GOBIN:$GOROOT/bin:$PATH'
+  fi
+  if ! $(go version); then err "err install go"; fi
 fi
 
 # install docker
