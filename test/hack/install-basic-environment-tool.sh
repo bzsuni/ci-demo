4058e2602af1903afe1098628c8dffcdbd78a39d
bzsuni <bingzhe.sun@daocloud.io>
2022-04-14 23:30:48 +0800

    setup kind cluster

--- test/hack/install-basic-environment-tool.sh
+++ test/hack/install-basic-environment-tool.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
-source ./helper.sh
+function msg() {
+   if [[ $# -ne 1 ]]; then echo "[func msg] one arg needed"; exit 1; fi
+    echo -e "\033[35m $1 \033[0m"
+}
+function err() {
+   if [[ $# -ne 1 ]]; then echo "[func err] one arg needed"; exit 1; fi
+   echo -e "\033[31m $1 \033[0m"
+}
+function succ() {
+   if [[ $# -ne 1 ]]; then echo "[func succ] one arg needed"; exit 1; fi
+   echo -e "\033[32m $1 \033[0m"
+}
+
 # install go
 msg "## install go"
 if ! $(go version > /dev/null 2>&1); then
